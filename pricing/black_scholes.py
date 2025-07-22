82ed446e8cd89d780896fc01dc78cb535b2f257d
Santino Di Tomás <santiditomas@gmail.com>
2025-07-22 13:57:20 -0300

    Add Delta function

--- pricing/black_scholes.py
+++ pricing/black_scholes.py
@@ -12,5 +12,17 @@ def black_scholes_price(S, K, T, r, sigma, option_type="call"):
     else:
         raise ValueError("Invalid option type")
     
+def delta(S, K, T, r, sigma, option_type="call"):
+    """
+    Delta is the first derivative of option price with respect to underlying price S.
+    """
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    if option_type == "call":
+        return norm.cdf(d1)
+    elif option_type == "put":
+        return 1 - norm.cdf(d1)
+    else:
+        raise ValueError("Invalid option type")
+    
 
 
